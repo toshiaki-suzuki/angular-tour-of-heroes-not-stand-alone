4137f0cbe497208948704733cebacc1d391c6c6c
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-27 13:35:56 +0900

    Add message history to heroes component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import type { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({ // コンポーネントにDI可能なことを示すデコレータ
   providedIn: 'root' // ルートインジェクタにサービスのプロバイダを登録
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,33 @@
 import { Component } from '@angular/core';
 import type { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
+
 export class HeroesComponent {
-  constructor(private heroService: HeroService) { } // コンストラクタでHeroServiceをDI
+
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
