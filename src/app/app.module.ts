f06f6766774eda4f5d487925bcf886ace90cbb9a
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-28 09:38:46 +0900

    Add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
