4ed0b3c4b87cbf80418cf8fffc0ca4663e88f07c
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-05-03 09:15:15 +0900

    Add updateHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // Web APIのURL
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -54,5 +58,12 @@ export class HeroService {
     );
   }
 
+  /** PUT: サーバー上でヒーローを更新 */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 
 }
