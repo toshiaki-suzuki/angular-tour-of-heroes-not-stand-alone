19552e4f900f9443472924122b7451a189680b3e
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-05-03 09:23:25 +0900

    Add addHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,4 +66,11 @@ export class HeroService {
     );
   }
 
+  /** POST: サーバーに新しいヒーローを登録する */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
